1a0aa0e62fa6b54c8c4bd9f79f97c935bdbeda6f
Admin <admin@VIL-S01-iMac-ITA-21.local>
2024-01-08 17:47:16 +0100

    création des tables de base de données

---
+++ database/migrations/2024_01_08_162114_create_requests_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_project');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('url');
+            $table->enum('status', ['queued', 'processing', 'error', 'finished']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('requests');
+    }
+};
