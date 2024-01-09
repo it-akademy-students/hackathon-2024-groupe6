695d3cfa090e05a2ceb982e332deff2db8809a50
admin <admin@VIL-S01-iMac-ITA-05.local>
2024-01-09 10:26:36 +0100

    Update des Models et ses relations

--- app/Models/Request.php
+++ app/Models/Request.php
@@ -4,8 +4,37 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Request extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name_project',
+        'url',
+        'status'=>'queued',
+    ];
+
+    /**
+     * Relationships between models User & Request
+     */
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo('User');
+    }
+
+    /**
+     * Relationships between models Result & Request
+     */
+    public function result() : HasOne
+    {
+        return $this->hasOne('Result');
+    }
 }
--- app/Models/Result.php
+++ app/Models/Result.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Result extends Model
 {
     use HasFactory;
+
+    /**
+     * Relationships between models Result & Request
+     */
+    public function request() : BelongsTo
+    {
+        return $this->belongsTo('Request');
+    }
 }
