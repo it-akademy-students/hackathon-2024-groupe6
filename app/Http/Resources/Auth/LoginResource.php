b32b4b27868637c0f85b44be221cb92c6f55b16e
Admin <admin@VIL-S01-iMac-ITA-21.local>
2024-01-09 16:28:40 +0100

    mise en place authentification Login route

---
+++ app/Http/Resources/Auth/LoginResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources\Auth;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class LoginResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "email" => $this->email,
+            "access_token" => $this->createToken("access_token")->plainTextToken,
+            "created_at" => $this->created_at,
+            "updated_at" => $this->updated_at,
+        ];
+    }
+}
