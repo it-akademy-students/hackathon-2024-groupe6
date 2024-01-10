ec9dc2a74eaf88e3a1efec494be9d09227f3d636
Admin <admin@VIL-S01-iMac-ITA-21.local>
2024-01-10 16:58:59 +0100

    ajout d'une route, demandController, DemandRequest, et RegisterDemandRessource en cours

---
+++ app/Http/Requests/Demand/DemandRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests\Demand;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DemandRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        if(auth('sanctum')->check()) return true;
+        return false;
+    }
+    public function rules(): array
+    {
+        return [
+            'name_project' => 'string|max:250',
+            'url' => 'required|string|regex:~^https://(github|gitlab)\.com/([\w.-]+)/([\w.-]+)\.git$~'
+        ];
+    }
+}
