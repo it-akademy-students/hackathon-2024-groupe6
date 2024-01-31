cea647c47a27a52a3f6c7d0295a4a43cd97432d9
eloiducrest <eloi@letmotiv.io>
2024-01-31 11:40:45 +0100

    finish settings

---
+++ database/migrations/2024_01_30_145232_add_columns_to_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('address')->nullable();
+            $table->string('zip_code')->nullable();
+            $table->string('city')->nullable();
+            $table->string('country')->nullable();
+            $table->string('tel')->nullable();
+            $table->longText('description')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('address');
+            $table->dropColumn('zip_code');
+            $table->dropColumn('city');
+            $table->dropColumn('country');
+            $table->dropColumn('tel');
+            $table->dropColumn('description');
+        });
+    }
+};
